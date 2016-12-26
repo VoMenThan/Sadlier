7401e1de4794f83176d56c43d4544ea4b958cf78
VoMenThan <thancn12@gmail.com>
2016-12-26 11:59:57 +0700

    no message

--- html/flashCards.php
+++ html/flashCards.php
@@ -1,15 +1,15 @@
 <?php include('header.php'); ?>
 <div class="wrap-page flag_cards">
     <div class="content">
-        <div class="flag"><img src="assets/images/icon_flag.jpg" alt="flag"></div>
+        <div class="flag"><img src="../assets/images/icon_flag.jpg" alt="flag"></div>
         <div class="box-content">
             to give advice or an opinion; to offer help <span ><i class="fa fa-address-book" ></i></span>
         </div>
         <div class="box-control">
             <ul class ="tool-control">
-                <li><img src="assets/images/btn_pre.jpg" alt=""></li>
-                <li><img src="assets/images/btn_refesh.jpg" alt=""></li>
-                <li><img src="assets/images/btn_next.jpg" alt=""></li>
+                <li><img src="../assets/images/btn_pre.jpg" alt=""></li>
+                <li><img src="../assets/images/btn_refesh.jpg" alt=""></li>
+                <li><img src="../assets/images/btn_next.jpg" alt=""></li>
             </ul>
 
         </div>

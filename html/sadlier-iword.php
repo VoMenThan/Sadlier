5e2b96b91f420978f7a96155d688267ec465588d
DESKTOP-12MHOQP\NT_Nhan <nhan.phan@hoanghacgroup.com>
2016-12-26 13:57:35 +0700

    a

--- html/sadlier-iword.php
+++ html/sadlier-iword.php
@@ -10,12 +10,26 @@
     </div>
     <div class="main-wrapper">
         <div class="select-wrapper">
-            <select>
+            <select class="clearfix">
                 <option value="volvo">Volvo</option>
                 <option value="saab">Saab</option>
                 <option value="mercedes">Mercedes</option>
                 <option value="audi">Audi</option>
             </select>
+        </div>
+        <div class="menu-wrapper text-center">
+            <ul class="menu">
+                <a href="#"><li class="item active">Set A</li></a>
+                <a href="#"><li class="item">Set B</li></a>
+                <a href="#"><li class="item">A & B</li></a>
+            </ul>
+        </div>
+        <div class="icon-wrapper text-center">
+            <ul class="menu">
+                <a href="#"><li class="item"><img src="assets/images/icon-info.jpg"></li></a>
+                <a href="#"><li class="item"><img src="assets/images/icon-sound.jpg"></li></a>
+                <a href="#"><li class="item"><img src="assets/images/icon-cham.jpg"></li></a>
+            </ul>
         </div>
     </div>
 </div>

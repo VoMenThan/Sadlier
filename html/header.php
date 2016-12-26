25b68ad02c5ac052954261d8be509632269d2cb7
DESKTOP-12MHOQP\NT_Nhan <nhan.phan@hoanghacgroup.com>
2016-12-26 17:38:28 +0700

    a

--- html/header.php
+++ html/header.php
@@ -18,9 +18,10 @@
                 <div class="main-wrapper clearfix">
                     <div class="icon-wrapper text-center">
                         <ul class="menu">
-                            <a href="#"><li class="item"><img src="assets/images/icon-info.jpg"></li></a>
-                            <a href="#"><li class="item"><img src="assets/images/icon-sound.jpg"></li></a>
-                            <a href="#"><li class="item"><img src="assets/images/icon-cham.png"></li></a>
+                            <li class="item logo-tm"><a href="#"><img src="assets/images/icon/logo.jpg"></a></li>
+                            <li class="item icon-one"><a href="#"><img src="assets/images/icon/icon-info.jpg"></a></li>
+                            <li class="item icon-two"><a href="#"><img src="assets/images/icon/icon-sound.jpg"></a></li>
+                            <li class="item icon-three"><a href="#"><img src="assets/images/icon/icon-cham.png"></a></li>
                         </ul>
                     </div>
                     <div class="box-menu clearfix">
@@ -40,7 +41,28 @@
                             </ul>
                         </div>
                     </div>
-
+                </div>
+                <div class="menu-box clearfix">
+                    <div class="box-prev">
+                        <img src="assets/images/icon/icon-prev.jpg" class="icon-three" style="cursor: pointer">
+                    </div>
+                    <div class="box-li">
+                        <ul class="menu">
+                            <li class="item active"><a href="#">Show by word</a>  <img src="assets/images/icon/icon-check.jpg" class="check"></li>
+                            <li class="item"><a href="#">Show by definition</a></li>
+                        </ul>
+                    </div>
+                    <div class="box-li">
+                        <ul class="menu">
+                            <li class="item active"><a href="#">Show flagged</a></li>
+                            <li class="item"><a href="#">Show all</a> <img src="assets/images/icon/icon-check.jpg" class="check"></li>
+                        </ul>
+                    </div>
+                    <div class="box-li">
+                    <ul class="menu">
+                        <li class="item active"><a href="#">Print</a></li>
+                    </ul>
+                    </div>
                 </div>
         </header>
 

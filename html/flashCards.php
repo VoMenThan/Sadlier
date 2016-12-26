4282df227f216f1bb4365bbca0ccc3e725bc8f79
VoMenThan <thancn12@gmail.com>
2016-12-26 13:57:18 +0700

    update

--- html/flashCards.php
+++ html/flashCards.php
@@ -1,17 +1,20 @@
 <?php include('header.php'); ?>
 <div class="wrap-page flag_cards">
-    <div class="content">
-        <div class="flag"><img src="../assets/images/icon_flag.jpg" alt="flag"></div>
-        <div class="box-content">
-            to give advice or an opinion; to offer help <span ><i class="fa fa-address-book" ></i></span>
-        </div>
-        <div class="box-control">
-            <ul class ="tool-control">
-                <li><img src="../assets/images/btn_pre.jpg" alt=""></li>
-                <li><img src="../assets/images/btn_refesh.jpg" alt=""></li>
-                <li><img src="../assets/images/btn_next.jpg" alt=""></li>
-            </ul>
+    <div class="page-number">1 of 8</div>
+    <div class="bg-white">
+        <div class="content">
+            <div class="flag"><a href="#"><img src="assets/images/icon_flag.jpg" alt=""></a></div>
+            <div class="box-content">
+                to give advice <br> or an opinion;<br> to offer help
+            </div>
+            <div class="box-control">
+                <ul class ="tool-control">
+                    <li class="control"><a href="#"><img src="assets/images/btn_pre.jpg" alt=""></a></li>
+                    <li class="control"><a href="#"><img src="assets/images/btn_refesh.jpg" alt=""></a></li>
+                    <li class="control"><a href="#"><img src="assets/images/btn_next.jpg" alt=""></a></li>
+                </ul>
 
+            </div>
         </div>
     </div>
 

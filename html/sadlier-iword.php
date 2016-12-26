7f1bb5ec3ea776fdc48b154764177aa08f254a8d
DESKTOP-12MHOQP\NT_Nhan <nhan.phan@hoanghacgroup.com>
2016-12-26 11:28:33 +0700

    a

--- html/sadlier-iword.php
+++ html/sadlier-iword.php
@@ -1,5 +1,6 @@
 <div class="container">
-    <div class="">
-
+    <div class="logo-title">
+        <img src="assets/images/icon-sound-logo.jpg">
+        <h3><span class="text-logo-color">Audio:</span> IWords</h3>
     </div>
 </div>

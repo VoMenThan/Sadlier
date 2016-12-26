3370abac7eb83b30adc4c1d7e71fed41aa380097
DESKTOP-12MHOQP\NT_Nhan <nhan.phan@hoanghacgroup.com>
2016-12-26 12:00:12 +0700

    a

--- html/sadlier-iword.php
+++ html/sadlier-iword.php
@@ -1,6 +1,22 @@
+<?php include('header.php'); ?>
 <div class="container">
-    <div class="logo-title">
-        <img src="assets/images/icon-sound-logo.jpg">
-        <h3><span class="text-logo-color">Audio:</span> IWords</h3>
+    <div class="heading-wrapper clearfix">
+        <div class="logo-title">
+            <img src="assets/images/icon-sound-logo.jpg">
+        </div>
+        <div class="text-title">
+            <h3><span class="text-logo-color">Audio:</span> IWords</h3>
+        </div>
+    </div>
+    <div class="main-wrapper">
+        <div class="select-wrapper">
+            <select>
+                <option value="volvo">Volvo</option>
+                <option value="saab">Saab</option>
+                <option value="mercedes">Mercedes</option>
+                <option value="audi">Audi</option>
+            </select>
+        </div>
     </div>
 </div>
+<?php include('footer.php'); ?>

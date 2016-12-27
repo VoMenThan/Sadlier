eabca3163854cc5d2e6c13c89619f8bbbdfa5538
Nguyễn Thái Hoài Nam <hoainamvn2000@gmail.com>
2016-12-27 12:00:32 +0700

    update iwords

--- html/sadlier-iword.php
+++ html/sadlier-iword.php
@@ -30,6 +30,62 @@
                     </div>
                 </div>
             </div>
+            <div class="lesson-show">
+                <div class="top-word clearfix">
+                    <div class="text">
+                        <div class="unit">Unit Passage Audio</div>
+                        <div class="desc">I'll wait for the  Movie (Compare-and-Contrast Essay)</div>
+                    </div>
+                    <div class="down text-center">
+                        <div class="lession">Unit 1</div>
+                        <a href=""><img src="assets/images/icon/down.jpg" alt=""></a>
+                    </div>
+                </div>
+                <div class="content-show clearfix">
+                    <p>
+                        <select>
+                            <option value="">admonish</option>
+                            <option value="">admonish</option>
+                            <option value="">admonish</option>
+                            <option value="">admonish</option>
+                        </select>
+                    </p>
+                    <p>circumspect</p>
+                    <p>deadlock</p>
+                    <p>circumspect</p>
+                    <p>deadlock</p>
+                    <p>circumspect</p>
+                    <p>deadlock</p>
+                    <p>circumspect</p>
+                    <p>deadlock</p>
+                    <p>
+                        <select>
+                            <option value="">admonish</option>
+                            <option value="">admonish</option>
+                            <option value="">admonish</option>
+                            <option value="">admonish</option>
+                        </select>
+                    </p>
+                    <p>circumspect</p>
+                    <p>deadlock</p>
+                    <p>circumspect</p>
+                    <p>deadlock</p>
+                    <p>circumspect</p>
+                    <p>deadlock</p>
+                    <p>circumspect</p>
+                    <p>deadlock</p>
+                    <p>circumspect</p>
+                    <p>
+                        <select>
+                            <option value="">admonish</option>
+                            <option value="">admonish</option>
+                            <option value="">admonish</option>
+                            <option value="">admonish</option>
+                        </select>
+                    </p>
+                    <p>deadlock</p>
+                </div>
+            </div>
         </div>
     </div>
     <script src="assets/js/audio.js"></script>

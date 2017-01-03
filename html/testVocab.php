17cca2ef87ac99fba2db5e38181b8d0a9a29490e
vomenthan <thancn12@gmail.com>
2017-01-03 11:09:27 +0700

    no message

--- html/testVocab.php
+++ html/testVocab.php
@@ -40,8 +40,8 @@
                     <div class="pupup-submit">
                         <div class="game-over">Game Over</div>
                         <div class="result">
-<!--                            <span class="good">Good Job!</span>-->
-                            <span class="bad">Bad Job!</span>
+                            <span class="good">Good Job!</span>
+<!--                            <span class="bad">Bad Job!</span>-->
                         </div>
                         <div class="score"> <span class="gray">Score:</span> 9400</div>
                         <div class="play-again">Play Again</div>
